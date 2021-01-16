6cc6e742916270260abf8b49ddad11fcad922b37
Jaiprakash siyyadri <jaiprakash.siyyadri@gramener.org>
2021-01-16 18:17:47 +0530

    UPD:

--- functions/index.js
+++ functions/index.js
@@ -3,27 +3,32 @@ const functions = require('firebase-functions');
 // var firebase = require('firebase');
 // var firebaseui = require('firebaseui');
 const express = require('express')
+const hbs = require("express-handlebars")
+const path = require("path")
 const cors = require('cors')({origin: true});
-
 const app = express()
-app.use(cors);
-
 const app2 = express()
 
-app2.get('/', (req, res) => {
-  res.send('Hello JP!')
-})
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app2.use(cors);
+app2.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'default_layout', layoutsDir: path.join(__dirname, 'src/layouts')}))
+app2.set('views', path.join(__dirname, "src/views"))
+app2.set('view engine', 'hbs')
+
+app2.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+// app2.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+// app2.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
+app2.get('/login', (req, res) => {
+  res.render('login')
 })
 
-app.get('/api', (req, res) => {
-  const date = new Date();
-  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-  res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-  res.set('Vary', 'Accept-Encoding, X-My-Custom-Header');
-  res.json({bongs: 'BONG '.repeat(hours)});
-});
+app2.get('/', (req, res) => {
+  res.render('index', {'title': 'console', 'name': 'jp', 'age': '25', 'condition': true, 'arrayN': [1,2,3]})
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World! ' + __dirname)
+})
 
 // The Firebase Admin SDK to access Cloud Firestore.
 // admin.initializeApp();
